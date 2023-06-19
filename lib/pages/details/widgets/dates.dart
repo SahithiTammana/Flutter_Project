7318fa1976b0e3d864e79c55958eb2e77c2859e1
deepak <nayaneedeepak@gmail.com>
2023-06-19 11:29:08 +0530

    Recent Activities and Date box were added.

--- lib/pages/details/widgets/dates.dart
+++ lib/pages/details/widgets/dates.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/helpers.dart';
 
 class Dates extends StatelessWidget {
   const Dates({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<DateBox> dateBoxes = [];
+
+    DateTime date = DateTime.parse('2021-11-20');
+
+    for (int i = 0; i < 6; i++) {
+      dateBoxes.add(DateBox(
+        date: date,
+        active: i == 3,
+      ));
+      date = date.add(Duration(days: 1));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: dateBoxes,
+      ),
+    );
+  }
+}
+
+class DateBox extends StatelessWidget {
+  final bool active;
+  final DateTime date;
+
+  const DateBox({
+    this.active = false,
+    required this.date,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        height: 90,
-        color: Color.fromARGB(255, 25, 226, 129));
+      width: 50,
+      height: 70,
+      decoration: BoxDecoration(
+        gradient: active
+            ? LinearGradient(colors: [
+                Color(0xff92e2ff),
+                Color(0xff1ebdf8),
+              ], begin: Alignment.topCenter)
+            : null,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Color(0xffe1e1e1),
+        ),
+      ),
+      child: DefaultTextStyle.merge(
+        style: active ? TextStyle(color: Colors.white) : null,
+        child: Column(
+          children: [
+            Text(
+              daysOfWeek[date.weekday]!,
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              date.day.toString().padLeft(2, '0'),
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
